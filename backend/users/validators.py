60e8476e435d58fcb69e127d089a3f00a761ead7
Ирена Бунина <pprct.bn@yandex.ru>
2023-06-20 19:25:51 +0500

    part2

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -4,12 +4,15 @@ from django.core.exceptions import ValidationError
 
 def validate_username(value):
     value_tmp = value.lower()
-    abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 'q', 's',
-           't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_']
+    abc = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
+           'n', 'o', 'p', 'r', 'q', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z',
+           '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_']
 
     for i in range(len(value)):
         if value_tmp[i] not in abc:
-            raise ValidationError(u'Недопустимые символы ( {} )'.format(value_tmp[i]))
+            raise ValidationError(u'Недопустимые символы ( {} )'.format(
+                value_tmp[i]))
+
 
 def validate_email(value):
     if not bool(re.match(r'^[\w.@+-]+$', value)):
