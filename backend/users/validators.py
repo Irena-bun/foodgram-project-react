36fc0a67d40b8f78076b5b857eea124792f1eff1
Ирена Бунина <pprct.bn@yandex.ru>
2023-06-24 13:50:20 +0500

    validators flake8

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -23,13 +23,13 @@ from django.core.exceptions import ValidationError
 
 def validate_username(username):
     bad_symbols = re.sub(r'\w', '', username)
-    if bad_symbols:        
+    if bad_symbols:
         raise ValidationError(f'Некорректное имя.'
-                               'Недопустимые символы: {bad_symbols}')
+                              f'Недопустимые символы: {bad_symbols}')
 
 
-def validate_mail(mail):
-    bad_symbols = re.sub(r'^[\w.@+-]+$', '', mail)
+def validate_email(email):
+    bad_symbols = re.sub(r'^[\w.@+-]+$', '', email)
     if bad_symbols:
         raise ValidationError(f'Некорректный email.'
-                               'Недопустимые символы: {bad_symbols}')
+                              f'Недопустимые символы: {bad_symbols}')
