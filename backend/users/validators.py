64db96c149c1a7670814cde55554ea4aac81cfaa
Ирена Бунина <pprct.bn@yandex.ru>
2023-06-25 15:47:07 +0500

    part2_review2

--- backend/users/validators.py
+++ backend/users/validators.py
@@ -5,11 +5,15 @@ from django.core.exceptions import ValidationError
 def validate_username(username):
     bad_symbols = re.sub(r'\w', '', username)
     if bad_symbols:
-        raise ValidationError(f'Недопустимые символы: {bad_symbols}')
+        raise ValidationError(f'Недопустимые символы '
+                              f'в имени: {bad_symbols}'
+                              )
 
 
 def validate_email(email):
     for m in email:
         bad_symbols = re.sub(r'^[\w.@+-]+$', '', m)
         if bad_symbols:
-            raise ValidationError(m, f'Недопустимые символы: {bad_symbols}')
+            raise ValidationError(f'Недопустимые символы '
+                                  f'в email: {bad_symbols}'
+                                  )
