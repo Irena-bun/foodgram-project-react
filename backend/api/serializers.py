050b4cb6b9261dfc82d05e872da19d4491bed76b
Ирена Бунина <pprct.bn@yandex.ru>
2023-06-26 18:53:15 +0500

    part2_review3

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -182,7 +182,7 @@ class ShowRecipeSerializer(serializers.ModelSerializer):
 #        ingredients = RecipeIngredient.objects.filter(
 #            recipe=obj).select_related('ingredient')
         ingredients = RecipeIngredient.objects.filter(
-            recipe=obj.ingredients)
+            recipe=obj.ingredients).select_related('ingredient')
         return ShowIngredientsInRecipeSerializer(ingredients, many=True).data
 
     def get_is_favorited(self, obj):
